e6df5ed6fe46796255d410dc743a0278f421a7a4
lietcooper <lietcooper123@gmail.com>
2026-02-16 00:26:46 -0800

    revised macOs-linux version

--- macOS-linux/chatServer.c
+++ macOS-linux/chatServer.c
@@ -22,49 +22,29 @@ int main() {
     char client_names[MAX_CLIENTS][50];
     int client_count = 0;
 
-    printf("=== 聊天服务器 (macOS) ===\n");
-    printf("正在初始化...\n");
+    printf("=== Chat Server (macOS/Linux) ===\n");
+    printf("Initializing...\n");
 
-    // 初始化客户端socket数组
     for (int i = 0; i < MAX_CLIENTS; i++) {
         client_sockets[i] = -1;
-        memset(client_names[i], 0, 50);
     }
 
-    // 创建服务器socket
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket < 0) {
-        perror("创建socket失败");
-        return 1;
-    }
-
-    // 设置地址重用，方便调试时重启服务器
     int opt = 1;
     setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    // 设置服务器地址
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    // 绑定socket
     if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("绑定失败");
-        close(server_socket);
+        perror("Bind failed");
         return 1;
     }
 
-    // 开始监听
-    if (listen(server_socket, MAX_CLIENTS) < 0) {
-        perror("监听失败");
-        close(server_socket);
-        return 1;
-    }
+    listen(server_socket, MAX_CLIENTS);
+    printf("Server listening on port %d\n", PORT);
 
-    printf("服务器启动成功，监听端口: %d\n", PORT);
-    printf("等待客户端连接...\n\n");
-
-    // 主循环
     while (1) {
         FD_ZERO(&read_fds);
         FD_SET(server_socket, &read_fds);
@@ -77,120 +57,55 @@ int main() {
             }
         }
 
-        struct timeval timeout;
-        timeout.tv_sec = 1;
-        timeout.tv_usec = 0;
+        select(max_sd + 1, &read_fds, NULL, NULL, NULL);
 
-        int activity = select(max_sd + 1, &read_fds, NULL, NULL, &timeout);
-
-        if (activity < 0 && errno != EINTR) {
-            perror("select错误");
-            break;
-        }
-
-        // 检查新连接
         if (FD_ISSET(server_socket, &read_fds)) {
             client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
-            if (client_socket < 0) {
-                perror("接受连接失败");
-                continue;
-            }
-
-            int client_index = -1;
+            int index = -1;
             for (int i = 0; i < MAX_CLIENTS; i++) {
                 if (client_sockets[i] == -1) {
                     client_sockets[i] = client_socket;
-                    client_index = i;
-                    client_count++;
+                    index = i;
                     break;
                 }
             }
 
-            if (client_index == -1) {
-                printf("服务器已满，拒绝新连接\n");
-                const char* msg = "服务器已满，请稍后重试\n";
-                send(client_socket, msg, strlen(msg), 0);
-                close(client_socket);
-                continue;
-            }
-
-            // 接收用户名
-            int name_len = recv(client_socket, client_names[client_index], sizeof(client_names[client_index]) - 1, 0);
-            if (name_len > 0) {
-                client_names[client_index][name_len] = '\0';
-                // 移除可能的换行符
-                if (client_names[client_index][name_len - 1] == '\n' || client_names[client_index][name_len - 1] == '\r') {
-                    client_names[client_index][name_len - 1] = '\0';
-                }
+            int n = recv(client_socket, client_names[index], 49, 0);
+            if (n > 0) {
+                client_names[index][n] = '\0';
+                client_names[index][strcspn(client_names[index], "\r\n")] = 0;
             } else {
-                strncpy(client_names[client_index], "匿名用户", 50);
+                strcpy(client_names[index], "Anonymous");
             }
 
-            printf("新客户端连接: %s (IP: %s)\n", client_names[client_index], inet_ntoa(client_addr.sin_addr));
-
-            char welcome_msg[BUFFER_SIZE];
-            snprintf(welcome_msg, sizeof(welcome_msg), "欢迎 %s 加入聊天室！当前在线用户: %d\n", client_names[client_index], client_count);
-            send(client_socket, welcome_msg, strlen(welcome_msg), 0);
-
-            char join_msg[BUFFER_SIZE];
-            snprintf(join_msg, sizeof(join_msg), "系统: %s 加入了聊天室\n", client_names[client_index]);
-            for (int i = 0; i < MAX_CLIENTS; i++) {
-                if (client_sockets[i] != -1 && i != client_index) {
-                    send(client_sockets[i], join_msg, strlen(join_msg), 0);
-                }
-            }
+            printf("New connection: %s\n", client_names[index]);
+            char welcome[BUFFER_SIZE];
+            sprintf(welcome, "Welcome %s!\n", client_names[index]);
+            send(client_socket, welcome, strlen(welcome), 0);
         }
 
-        // 检查客户端消息
         for (int i = 0; i < MAX_CLIENTS; i++) {
-            if (client_sockets[i] == -1) continue;
+            if (client_sockets[i] != -1 && FD_ISSET(client_sockets[i], &read_fds)) {
+                int n = recv(client_sockets[i], buffer, BUFFER_SIZE - 1, 0);
+                if (n <= 0) {
+                    printf("%s disconnected\n", client_names[i]);
+                    close(client_sockets[i]);
+                    client_sockets[i] = -1;
+                } else {
+                    buffer[n] = '\0';
+                    buffer[strcspn(buffer, "\r\n")] = 0;
+                    printf("%s: %s\n", client_names[i], buffer);
 
-            if (FD_ISSET(client_sockets[i], &read_fds)) {
-                int bytes_received = recv(client_sockets[i], buffer, BUFFER_SIZE - 1, 0);
-
-                if (bytes_received <= 0) {
-                    printf("客户端断开: %s\n", client_names[i]);
-                    char leave_msg[BUFFER_SIZE];
-                    snprintf(leave_msg, sizeof(leave_msg), "系统: %s 离开了聊天室\n", client_names[i]);
-
+                    char msg[BUFFER_SIZE + 100];
+                    sprintf(msg, "%s: %s\n", client_names[i], buffer);
                     for (int j = 0; j < MAX_CLIENTS; j++) {
                         if (client_sockets[j] != -1 && j != i) {
-                            send(client_sockets[j], leave_msg, strlen(leave_msg), 0);
+                            send(client_sockets[j], msg, strlen(msg), 0);
                         }
-                    }
-
-                    close(client_sockets[i]);
-                    client_sockets[i] = -1;
-                    client_count--;
-                    continue;
-                }
-
-                buffer[bytes_received] = '\0';
-                if (buffer[bytes_received - 1] == '\n') buffer[bytes_received - 1] = '\0';
-                if (buffer[bytes_received - 1] == '\r') buffer[bytes_received - 1] = '\0';
-
-                printf("%s: %s\n", client_names[i], buffer);
-
-                if (strcmp(buffer, "quit") == 0 || strcmp(buffer, "exit") == 0) {
-                    const char* goodbye = "再见！\n";
-                    send(client_sockets[i], goodbye, strlen(goodbye), 0);
-                    close(client_sockets[i]);
-                    client_sockets[i] = -1;
-                    client_count--;
-                    continue;
-                }
-
-                char broadcast_msg[BUFFER_SIZE + 100];
-                snprintf(broadcast_msg, sizeof(broadcast_msg), "%s: %s\n", client_names[i], buffer);
-                for (int j = 0; j < MAX_CLIENTS; j++) {
-                    if (client_sockets[j] != -1 && j != i) {
-                        send(client_sockets[j], broadcast_msg, strlen(broadcast_msg), 0);
                     }
                 }
             }
         }
     }
-
-    close(server_socket);
     return 0;
 }
--- macOS-linux/client.c
+++ macOS-linux/client.c
@@ -6,131 +6,70 @@
 #include <sys/socket.h>
 #include <pthread.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-// 接收消息的线程函数
 void* ReceiveThread(void* socket_ptr) {
     int client_socket = *(int*)socket_ptr;
     char buffer[BUFFER_SIZE];
 
     while (1) {
-        int bytes_received = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
-
-        if (bytes_received > 0) {
-            buffer[bytes_received] = '\0';
-            printf("%s", buffer);
-            printf("> ");
+        int n = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
+        if (n > 0) {
+            buffer[n] = '\0';
+            printf("\r%s> ", buffer);
             fflush(stdout);
-        }
-        else if (bytes_received == 0) {
-            printf("\n服务器连接已断开\n");
+        } else if (n == 0) {
+            printf("\nServer disconnected\n");
+            break;
+        } else if (errno != EAGAIN && errno != EWOULDBLOCK) {
             break;
         }
-        else {
-            // 在非阻塞模式下，需要忽略 EAGAIN 和 EWOULDBLOCK
-            usleep(100000); // 100ms
-        }
+        usleep(100000);
     }
-
     return NULL;
-}
-
-int GetConsoleInput(char* buffer, int buffer_size) {
-    if (fgets(buffer, buffer_size, stdin) == NULL) {
-        return 0;
-    }
-
-    int len = (int)strlen(buffer);
-    if (len > 0 && buffer[len - 1] == '\n') {
-        buffer[len - 1] = '\0';
-        len--;
-    }
-    if (len > 0 && buffer[len - 1] == '\r') {
-        buffer[len - 1] = '\0';
-        len--;
-    }
-
-    return len;
 }
 
 int main() {
     int client_socket;
     struct sockaddr_in server_addr;
-    char buffer[BUFFER_SIZE];
-    char username[50];
-    pthread_t receive_thread;
+    char buffer[BUFFER_SIZE], username[50];
+    pthread_t thread;
 
-    printf("=== 聊天客户端 (macOS) ===\n\n");
-
-    printf("请输入您的用户名: ");
-    fflush(stdout);
-
-    int username_len = GetConsoleInput(username, sizeof(username));
-
-    if (username_len == 0) {
-        strncpy(username, "匿名用户", 50);
-    }
-
-    printf("正在连接到服务器...\n");
+    printf("=== Chat Client (macOS/Linux) ===\n");
+    printf("Enter username: ");
+    fgets(username, 50, stdin);
+    username[strcspn(username, "\r\n")] = 0;
 
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket < 0) {
-        perror("创建socket失败");
-        return 1;
-    }
-
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("连接服务器失败");
-        close(client_socket);
+        perror("Connect failed");
         return 1;
     }
 
-    // 立即发送用户名
     send(client_socket, username, strlen(username), 0);
-
-    printf("连接成功！\n");
-    printf("输入消息开始聊天，输入 'quit' 退出\n\n");
-
-    // 设置为非阻塞模式
+    
     int flags = fcntl(client_socket, F_GETFL, 0);
     fcntl(client_socket, F_SETFL, flags | O_NONBLOCK);
 
-    // 创建接收线程
-    if (pthread_create(&receive_thread, NULL, ReceiveThread, &client_socket) != 0) {
-        perror("创建接收线程失败");
-        close(client_socket);
-        return 1;
-    }
+    pthread_create(&thread, NULL, ReceiveThread, &client_socket);
 
-    // 主循环 - 发送消息
     while (1) {
         printf("> ");
         fflush(stdout);
+        fgets(buffer, BUFFER_SIZE, stdin);
+        buffer[strcspn(buffer, "\r\n")] = 0;
 
-        int input_len = GetConsoleInput(buffer, sizeof(buffer));
-
-        if (input_len == 0) {
-            continue;
-        }
-
-        if (strcmp(buffer, "quit") == 0 || strcmp(buffer, "exit") == 0) {
-            send(client_socket, buffer, strlen(buffer), 0);
-            break;
-        }
-
-        if (send(client_socket, buffer, strlen(buffer), 0) < 0) {
-            perror("发送失败");
-            break;
-        }
+        if (strcmp(buffer, "quit") == 0) break;
+        send(client_socket, buffer, strlen(buffer), 0);
     }
 
     close(client_socket);
-    printf("客户端已退出\n");
     return 0;
 }
